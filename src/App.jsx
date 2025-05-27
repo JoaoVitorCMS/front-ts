e0f9c564e5c832c5307cc4af1f32e4d1aca9ad39
João Vitor Costa de Moura Santos <joaovitorcms1@gmail.com>
2025-05-27 16:43:29 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,58 @@
 
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { api } from './api/api'
-import { Navigate,useNavigate } from 'react-router'
-import Style from './App.module.css'
+import { useNavigate } from 'react-router'
+import Style from './App.module.css';
+
+
 
 function App() {
   const navigete = useNavigate();
 
-  const[email, setEmail] = useState('')
-  const[password, setPassword] = useState('')
-  const[user, setUser] = useState(null)
-  const[message, setMessage] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user')
-    if(storedUser){
+    if (storedUser) {
       setUser(JSON.parse(storedUser))
       navigete('/userslist')
     }
   }, [navigete])
-  const handleLogin = async (e) =>{
+  const handleLogin = async (e) => {
     e.preventDefault()
-    try{
-      const response = await api.post('/login', {email, password})
+    try {
+      const response = await api.post('/login', { email, password })
       const user = response.data
 
       localStorage.setItem('user', JSON.stringify(user))
       navigete('/usersList')
-    }catch(error){
+    } catch (error) {
       setMessage('Erro ao fazer login' + (error.response?.data?.message || 'verifique seus dados'))
     }
   }
 
   return (
     <>
-      <div style={{padding: '2rem'}}>
-        <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <input type="password" placeholder='Senha' value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <button type="submit">Entrar</button>
-        <p>{message}</p>
-        </form>
+      <div className={Style.wrapLogin}>
+        <div className={Style.wrapImg}>
+          <div className={Style.wrapDegrade}>
+
+          </div>
+        </div>
+        <div className={Style.wrapForm}>
+          <form onSubmit={handleLogin}>
+            <h2>Login</h2>
+            <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="password" placeholder='Senha' value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="submit">Entrar</button>
+            <p>{message}</p>
+          </form>
+        </div>
       </div>
+
     </>
   )
 }
